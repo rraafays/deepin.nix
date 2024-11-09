36f4eff461a565e2eaf986ba591ab4659aeb7b78
raf <rraf@tuta.io>
2024-11-09 18:31:56 +0000

    chore: initialised repo

---
+++ default.nix
@@ -0,0 +1,9 @@
+{ ... }:
+
+{
+  services.desktopManager.deepin.enable = true;
+  services.deepin.deepin-anything.enable = true;
+  services.deepin.dde-daemon.enable = true;
+  services.deepin.dde-api.enable = true;
+  services.deepin.app-services.enable = true;
+}
